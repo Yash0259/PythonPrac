2ac5e9b55ea60b3f4c61af364a929795b0bcc8c6
Yash0259 <bhamreyash2002.in>
2025-04-02 19:46:08 +0530

    Added begin

---
+++ begin.py
@@ -0,0 +1,17 @@
+print("what is your name =")
+name = input()
+print("Good Morning ",name)
+
+age = 23
+print(name + str(age))
+
+pyid= ["driv","pan","adhar"]
+dgid= ["email","phone"]
+
+id = pyid 
+print(id)
+pyid.append("cert")
+dgid.insert(1,"app")
+id = pyid + dgid
+print(id)
+
