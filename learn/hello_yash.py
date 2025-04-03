45313b32bde273a06142e6c0df8aa54743a6e318
Yash0259 <bhamreyash2002.in>
2025-04-03 20:01:30 +0530

    added latest files

---
+++ learn/hello_yash.py
@@ -0,0 +1,7 @@
+print("Hello Yash ");
+
+def name(c) : print(c);
+
+name_one = "Sash";
+
+name_two = "Raj";                       
---
+++ learn/use_name.py
@@ -0,0 +1,14 @@
+from hello_yash import name;
+
+name("Yash");
+
+age = int(input("Enter Your Age :"))
+
+if age < 13 :
+        print("child")
+elif age <20 :
+        print("adult")
+elif age < 45 :
+        print("senior")
+else age < 60 :
+        print("professor")
