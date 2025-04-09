a88eaa5b6e9651fa9cd71bbb5a6028b2454cc0b0
Yash0259 <bhamreyash2002@gmail.com>
2025-04-09 23:02:02 +0530

    add conditions

---
+++ conditions/01_if.py
@@ -0,0 +1,13 @@
+age = int(input("Enter Your Age :"))
+
+if age < 18 :
+        print("child")
+elif age <30 :
+        print("adult")
+elif age < 45 :
+        print("senior")
+else :
+        print("professor")
+
+
+
---
+++ conditions/02_if_and.py
@@ -0,0 +1,10 @@
+age = int(input("enter num : "))
+day = "wednesday"
+
+price = 12 if (age>=18) else 8
+
+if day == "wednesday" :
+	price -= 2 
+	#price = price -2
+	print("your cost is $",price)
+
