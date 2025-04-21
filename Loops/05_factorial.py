cd4c60360e2106fc0fc94bddf602cc899c5e4d70
Yash0259 <bhamreyash2002@gmail.com>
2025-04-21 23:19:18 +0530

    added factorial

---
+++ Loops/05_factorial.py
@@ -0,0 +1,8 @@
+num = 5 
+factorial = 1 #start from one always
+
+while num > 0 : #always start with true statement 
+	factorial = factorial * num		#also can use factorial *= num
+	num = num -1 
+
+print(factorial)
